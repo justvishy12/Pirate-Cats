ce04eaedda567946cc556d35a4c0a4f6c3a14b8d
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2026-07-29 15:51:34 -0500

    Update starpuzzle.gd

--- pirate-cats/script/starpuzzle.gd
+++ pirate-cats/script/starpuzzle.gd
@@ -141,7 +141,7 @@ func _input(event):
 				$IslandInDistance.visible = false
 			if dialogue_index == 6:
 				$Textbox.visible = false
-				get_tree().change_scene_to_file("res://scene/sand.tscn")
+				get_tree().change_scene_to_file("res://scene/island.tscn")
 				
 			if dialogue_index == 4:
 				print("Hiding panel")
